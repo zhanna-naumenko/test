052060c66a397059914da41213449103b5aa4a83
Zhanna Naumenko <jannetj88@gmail.com>
2020-12-13 21:12:18 +0200

    added some changes to home_work_8_naumenko.py

--- home_work_8_naumenko.py
+++ home_work_8_naumenko.py
@@ -3,6 +3,7 @@ import io
 import os
 import random
 
+
 def open_file_domains():
     """Opens file domains.txt and return domains names without dot"""
     with open("D:/IT/Phyton/test/domains.txt", "r") as file_domains:
@@ -27,12 +28,12 @@ open_file_names()
 ###########################################################
 
 def random_domain():
-    """Opens file domains.txt and return random domain name without dot"""
+    """Opens file domains.txt and return random domain name"""
     with open("D:/IT/Phyton/test/domains.txt", "r") as file_domains:
         domain_name = []
         for line in file_domains.readlines():
-            domain_name.append(line.replace(".", "").strip())
-    print(random.choice(domain_name))
+            domain_name.append(line)
+    return random.choice(domain_name)
 
 def random_name():
     """Opens file names.txt and return random surname"""
@@ -40,14 +41,20 @@ def random_name():
         surname = []
         for line in data_persons.readlines():
             surname.append(line.split()[1])
-    print(random.choice(surname))
+    return random.choice(surname)
+
+def random_number():
+    """Create random number"""
+    return random.randint(100, 1000)
 
 
-random_number = random.randint(100, 1000)
-random_word = "".join(chr(randint(ord("a"), ord("z"))) for _ in range(randint(5, 7)))
+def random_word():
+    """Create random word"""
+    return "".join(chr(randint(ord("a"), ord("z"))) for _ in range(randint(5, 7)))
 
 
+def random_email():
+    """Create random e-mail"""
+    print(random_name() + "." + str(random_number()) + "@" + random_word() + random_domain())
 
-email = random_name() + "." + str(random_number) + "@" + str(random_word) + "." + random_domain()
-print(email)
-
+random_email()
